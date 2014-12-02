a7087833d36fe5ed5cbae1a98a4a36647a99b7a3
Abigail Stevens <A.L.Stevens@uva.nl>
2014-12-02 18:20:22 +0100

    Handles fits input.

--- plot_powerspec.py
+++ plot_powerspec.py
@@ -1,18 +1,12 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy.io import fits
 
 """
 		plot_powerspec.py
 
-Plots a power spectrum.
-
-Required arguments:
-tab_file - str - Name of file holding table of data (output from powerspec.py).
-
-Optional arguments:
-plot_file - str - Name of file which the plot will be saved to in this script.
-propID - str - The proposal ID of the data.
+Plots a power spectrum in the frequency domain.
 
 Written in Python 2.7 by A.L. Stevens, A.L.Stevens@uva.nl, 2013-2014
 
@@ -26,22 +20,33 @@ if __name__ == "__main__":
 	
 	parser = argparse.ArgumentParser(description="Plots a power spectrum from \
 		a data table.")
-	parser.add_argument('tab_file', help="The input table file, in \
-		ASCII/txt/dat format, with frequency in column 1, fractional rms^2 \
-		power in column 2, and error on power in column 3.")
+	parser.add_argument('tab_file', help="The table file, in .dat or .fits \
+		format, with frequency in column 1, fractional rms^2 power in column 2,\
+		and error on power in column 3.")
 	parser.add_argument('-o', '--outfile', nargs='?', dest='plot_file', \
 		default="./plot.png", help="The output file name for the power spectrum\
 		plot.")
-	parser.add_argument('-p', '--propid', nargs='?', dest='propID', \
+	parser.add_argument('-p', '--propID', nargs='?', dest='propID', \
 		default="Pxxxxx", help="The proposal ID of the data.")
 	args = parser.parse_args()
 	
 	print "Plotting the power spectrum: %s" % args.plot_file
 	
-	table = np.loadtxt(args.tab_file, comments='#')
-	freq = np.asarray(table[:,0])  # frequency, in Hz
-	rms2 = np.asarray(table[:,1])  # fractional rms^2 power
-	error = np.asarray(table[:,2])  # error on avg power
+	if args.tab_file[-4:].lower() == ".dat":
+		table = np.loadtxt(args.tab_file, comments='#')
+		freq = np.asarray(table[:,0])  # frequency, in Hz
+		rms2 = np.asarray(table[:,1])  # fractional rms^2 power
+		error = np.asarray(table[:,2])  # error on power
+	elif args.tab_file[-5:].lower() == ".fits":
+		file_hdu = fits.open(args.tab_file)
+		table = file_hdu[1].data
+		file_hdu.close()
+		freq = table.field('FREQUENCY')  # frequency, in Hz
+		rms2 = table.field('POWER')  # fractional rms^2 power
+		error = table.field('ERROR')  # error on power
+	else:
+		raise Exception('ERROR: File type not recognized. Must have extension .dat or .fits.')
+		
 	vpv = freq * rms2
 
 	fig, ax = plt.subplots()
