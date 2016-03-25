e3b1140935e78938a67d4c1a38bd0f538e26a6e9
Abigail Stevens <A.L.Stevens@uva.nl>
2016-03-24 20:44:25 -0700

    Attempting to package my code

---
+++ power_spectra/__init__.py
@@ -0,0 +1,15 @@
+# Licensed under an MIT license - see LICENSE.md
+
+"""
+Averaged power spectra of RXTE data.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from example_mod import *
